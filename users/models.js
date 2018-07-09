935190649b27d5895e3f02513e219f35749c6f39
Sayed Khan <arsalon47@gmail.com>
2018-07-09 12:17:43 -0400

    test

--- users/models.js
+++ users/models.js
@@ -1,6 +1,7 @@
 'use strict'; 
 
 const mongoose = require ('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = mongoose.Schema({
 
@@ -12,19 +13,19 @@ const userSchema = mongoose.Schema({
 
 userSchema.set('toObject', {
   transform: function (doc,ret) {
-    ret.id = red._id;
+    ret.id = ret._id;
     delete ret._id;
     delete ret._v;
     delete ret.password;
   }
 });
 
-// userSchema.methods.validatePassword = function (password) {
-//   return bcrypt.compare(password, this.password);
-// };
+userSchema.methods.validatePassword = function (password) {
+  return bcrypt.compare(password, this.password);
+};
 
-// userSchema.statics.hashPassword = function (password) {
-//   return bcrypt.hash(password, 10);
-// };
+userSchema.statics.hashPassword = function (password) {
+  return bcrypt.hash(password, 10);
+};
 
 module.exports = mongoose.model('User', userSchema);
