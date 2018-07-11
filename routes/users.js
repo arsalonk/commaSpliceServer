59f6552e92d30fbed72cc012f655b4901c63fb8a
Sayed Khan <arsalon47@gmail.com>
2018-07-11 17:14:25 -0400

    updated user route

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,9 @@
 
 const express = require('express');
 const router = express.Router();
+const passport = require('passport')
 
+router.use('/', passport.authenticate('jwt', { session: false, failWithError: true }));
 
 const User = require('../models/user')
 
